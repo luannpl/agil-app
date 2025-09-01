b8fae00b519af0d8db14a0544504d7cec1e5f1da
Paulo Luan <pauloluan1409@gmail.com>
2025-09-01 19:33:59 -0300

    feat: ajuste para pegar em ios

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.supabase.co", 
+        hostname: "**.supabase.co",
       },
     ],
   },
