3a0633f262bbd7dd9342ca16ee8c5fde2619faa4
Paulo Luan <pauloluan1409@gmail.com>
2025-09-01 19:49:07 -0300

    feat: ajuste para pegar em ios

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: "https://agil-api.onrender.com/api/:path*",
       },
     ];
   },
