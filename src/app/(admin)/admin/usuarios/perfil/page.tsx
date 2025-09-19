242641ab23685963101e0ce98723dcad274cee59
Paulo Luan <pauloluan1409@gmail.com>
2025-09-19 16:13:26 -0300

    feat(admin): perfil do usuário

--- src/app/(admin)/admin/usuarios/perfil/page.tsx
+++ src/app/(admin)/admin/usuarios/perfil/page.tsx
@@ -1,7 +1,61 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useAuth } from "@/contexts/AuthContext";
+import Link from "next/link";
+
 export default function PerfilPage() {
+  const { user } = useAuth();
+
+  if (!user) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Skeleton className="h-32 w-32 rounded-full" />
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Perfil do Usuário</h1>
+    <div className="p-4 bg-gray-600/5 rounded-lg backdrop-blur-md pb-8">
+      <h1 className="text-2xl font-bold mb-4">Perfil do Usuário</h1>
+      <div className="space-y-4">
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div className="w-full">
+            <Label className="mb-2 px-1 text-md">Nome </Label>
+            <Input value={user.nome} readOnly />
+          </div>
+          <div className="w-full">
+            <Label className="mb-2 px-1 text-md">Email</Label>
+            <Input value={user.email} readOnly />
+          </div>
+          <div className="w-full">
+            <Label className="mb-2 px-1 text-md">Tipo de Usuário</Label>
+            <Input value={user.tipo} readOnly />
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div className="w-full">
+            <Link href="/admin/veiculos/view" className="w-full">
+              <Button className="w-full" variant="secondary">
+                Voltar
+              </Button>
+            </Link>
+          </div>
+          <div className="w-full">
+            <Link href="/admin/veiculos/view" className="w-full">
+              <Button className="w-full" variant="secondary">
+                Alterar Senha
+              </Button>
+            </Link>
+          </div>
+          <div className="w-full">
+            <Button variant="auth" className="w-full">
+              Editar
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
