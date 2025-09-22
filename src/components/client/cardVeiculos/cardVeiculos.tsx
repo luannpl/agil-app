db35b29c5b69527967a4aca8e1436ec5b5f86a51
Paulo Luan <pauloluan1409@gmail.com>
2025-09-22 19:33:21 -0300

    feat: ajustes na home e add switch ao admin

--- src/components/client/cardVeiculos/cardVeiculos.tsx
+++ src/components/client/cardVeiculos/cardVeiculos.tsx
@@ -19,7 +19,7 @@ export default function CardVeiculos(props: Veiculo) {
         <Card className="h-full overflow-hidden hover:shadow-xl hover:shadow-yellow-500/20 transition-shadow duration-300 bg-gradient-to-br from-zinc-900 to-zinc-800 ">
           <div className="relative aspect-[4/3] overflow-hidden">
             <Image
-              src={props.imagem || "https://via.placeholder.com/400x300"}
+              src={props.imagem || "/agil-logo.png"}
               alt={props.nome}
               fill
               className="object-cover transition-transform duration-300 hover:scale-105"
--- src/components/client/hero-section/hero-section.tsx
+++ src/components/client/hero-section/hero-section.tsx
@@ -1,34 +1,152 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+"use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+import { Carousel } from "antd";
 
 export default function HeroSection() {
-  
-    return (
-        <>
-            <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8">
-                <div className="text-center space-y-4">
-                    <h1 className="text-gray-400 font-bold text-4xl md:text-5xl">
-                        Compre seu veículo com
-                         <span className="text-yellow-500"> Ágil Veiculos</span>
-                    </h1>
-                    <p className="text-gray-400 max-w-xl mx-auto leading-relaxed">
-                    Loja Automotiva para venda, troca, compra e financiamento de Automóveis
-                    </p>
-                </div>
-                <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
-                    <Link href="/veiculos">
-                    <Button className="w-full sm:w-auto mb-2" variant="auth" size="lg">
-                        Ver Veiculos
-                    </Button>
-                    </Link>
-                    <Link href="https://wa.me//5585921644075?text=Tenho%20interesse%20em%20comprar%20um%20carro">
-                    <Button className="w-full sm:w-auto mb-2" variant="outline" size="lg">
-                        Comprar
-                    </Button>
-                    </Link>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="mt-24 mx-auto w-full pb-4 px-4 sm:px-8">
+        <Carousel autoplay autoplaySpeed={4000} dots={false}>
+          <div>
+            <div>
+              <div className="text-center space-y-4">
+                <h1 className="text-gray-400 font-bold text-4xl md:text-5xl">
+                  Compre seu veículo com
+                  <span className="text-yellow-500"> Ágil Veiculos</span>
+                </h1>
+                <p className="text-gray-400 max-w-xl mx-auto leading-relaxed">
+                  Loja Automotiva para venda, troca, compra e financiamento de
+                  Automóveis
+                </p>
+              </div>
+              <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+                <Link href="/veiculos">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="auth"
+                    size="lg"
+                  >
+                    Ver Veiculos
+                  </Button>
+                </Link>
+                <Link href="https://wa.me//5585921644075?text=Tenho%20interesse%20em%20comprar%20um%20carro">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="outline"
+                    size="lg"
+                  >
+                    Comprar
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="text-center space-y-4">
+                <h1 className="text-gray-400 font-bold text-4xl md:text-5xl">
+                  Compre seu veículo com
+                  <span className="text-yellow-500"> Ágil Veiculos</span>
+                </h1>
+                <p className="text-gray-400 max-w-xl mx-auto leading-relaxed">
+                  Loja Automotiva para venda, troca, compra e financiamento de
+                  Automóveis
+                </p>
+              </div>
+              <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+                <Link href="/veiculos">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="auth"
+                    size="lg"
+                  >
+                    Ver Veiculos
+                  </Button>
+                </Link>
+                <Link href="https://wa.me//5585921644075?text=Tenho%20interesse%20em%20comprar%20um%20carro">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="outline"
+                    size="lg"
+                  >
+                    Comprar
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="text-center space-y-4">
+                <h1 className="text-gray-400 font-bold text-4xl md:text-5xl">
+                  Compre seu veículo com
+                  <span className="text-yellow-500"> Ágil Veiculos</span>
+                </h1>
+                <p className="text-gray-400 max-w-xl mx-auto leading-relaxed">
+                  Loja Automotiva para venda, troca, compra e financiamento de
+                  Automóveis
+                </p>
+              </div>
+              <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+                <Link href="/veiculos">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="auth"
+                    size="lg"
+                  >
+                    Ver Veiculos
+                  </Button>
+                </Link>
+                <Link href="https://wa.me//5585921644075?text=Tenho%20interesse%20em%20comprar%20um%20carro">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="outline"
+                    size="lg"
+                  >
+                    Comprar
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="text-center space-y-4">
+                <h1 className="text-gray-400 font-bold text-4xl md:text-5xl">
+                  Compre seu veículo com
+                  <span className="text-yellow-500"> Ágil Veiculos</span>
+                </h1>
+                <p className="text-gray-400 max-w-xl mx-auto leading-relaxed">
+                  Loja Automotiva para venda, troca, compra e financiamento de
+                  Automóveis
+                </p>
+              </div>
+              <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+                <Link href="/veiculos">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="auth"
+                    size="lg"
+                  >
+                    Ver Veiculos
+                  </Button>
+                </Link>
+                <Link href="https://wa.me//5585921644075?text=Tenho%20interesse%20em%20comprar%20um%20carro">
+                  <Button
+                    className="w-full sm:w-auto mb-2"
+                    variant="outline"
+                    size="lg"
+                  >
+                    Comprar
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Carousel>
+      </section>
+    </>
+  );
 }
